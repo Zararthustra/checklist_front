b0b38f65390f1940d8b652f00b322ce1f0d97dae
Zararthustra <70105093+Zararthustra@users.noreply.github.com>
2022-12-07 19:46:16 +0100

    Update host.js

--- src/utils/host.js
+++ src/utils/host.js
@@ -1,2 +1,2 @@
-// export const Host = "http://35.180.114.240/api";
-export const Host = "http://192.168.1.44:8000/api";
+export const Host = "http://35.180.114.240/api";
+// export const Host = "http://192.168.1.44:8000/api";
